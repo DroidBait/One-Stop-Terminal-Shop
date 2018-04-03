fb41ad6b3b7b852e5dfaf762f2eca2e107459915
Stephen Dickason <dickasonstephen@gmail.com>
2018-04-03 22:56:24 +0100

    Adding another way of creating the menu. I just cannot decide how best to create the menu

--- menu2.cpp
+++ menu2.cpp
@@ -2,7 +2,21 @@
 #include <vector>
 #include <string>
 
+using namespace std;
+
+void printmenu();
+
 int main(int argc, char *argv[])
 {
   std::vector<string> List;
+
+  printmenu();
+}
+
+void printmenu()
+{
+  cout << "1. First";
+  cout << "2. Second";
+  cout << "3. Third";
+  cout << "4. Close";
 }
